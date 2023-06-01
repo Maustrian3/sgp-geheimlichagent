0aa2318de06a195e8a927746d0097cfc58d1c86c
Oliver Mayer <e12023147@student.tuwien.ac.at>
2023-06-01 12:44:55 +0200

    Moved class

--- src/main/java/geheimlichagent/ActionHelper.java
+++ src/main/java/geheimlichagent/ActionHelper.java
@@ -1,4 +1,4 @@
-package helper;
+package geheimlichagent;
 
 import heimlich_and_co.actions.HeimlichAndCoAgentMoveAction;
 import heimlich_and_co.enums.Agent;
@@ -13,7 +13,7 @@ public class ActionHelper {
         String[] parts = action.toString().split("[:;]");
 
         // Überspringen des ersten Teils "AgentMoveAction"
-        for (int i = 1; i < parts.length; i += 2) {
+        for (int i = 1; i < parts.length - 1; i += 2) {
             String nameStr = parts[i].trim();
             String valueStr = parts[i + 1].trim();
 
