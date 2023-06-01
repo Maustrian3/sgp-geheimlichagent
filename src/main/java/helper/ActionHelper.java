6074a1b89e72c692eee3fd79817af2e41f479496
Oliver Mayer <e12023147@student.tuwien.ac.at>
2023-06-01 12:24:11 +0200

    Implemented a helper Class to extract the Information from MoveActions
    Action to string and extracting the information by parsing the string

---
+++ src/main/java/helper/ActionHelper.java
@@ -0,0 +1,27 @@
+package helper;
+
+import heimlich_and_co.actions.HeimlichAndCoAgentMoveAction;
+import heimlich_and_co.enums.Agent;
+
+import java.util.EnumMap;
+
+public class ActionHelper {
+
+    public static EnumMap<Agent, Integer> getAgentMovesFromMoveAction(HeimlichAndCoAgentMoveAction action) {
+        EnumMap<Agent, Integer> agentMoves = new EnumMap<>(Agent.class);
+
+        String[] parts = action.toString().split("[:;]");
+
+        // Überspringen des ersten Teils "AgentMoveAction"
+        for (int i = 1; i < parts.length; i += 2) {
+            String nameStr = parts[i].trim();
+            String valueStr = parts[i + 1].trim();
+
+            Agent name = Agent.valueOf(nameStr);
+            int value = Integer.parseInt(valueStr);
+
+            agentMoves.put(name, value);
+        }
+        return agentMoves;
+    }
+}
